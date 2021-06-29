60c9c68e0bf059cab36c3d441edca9a0d4b73da7
akbaryossy <akbaryossy@webmail.umm.ac.id>
2021-06-30 01:47:24 +0800

    Tubes

--- adminbaru/product/hapusproduct.php
+++ adminbaru/product/hapusproduct.php
@@ -1,6 +1,6 @@
 <?php
 // include database connection file
-$db_connection = mysqli_connect("localhost","root","","latihan");
+$db_connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b42fa1abdb","0ce40f57","heroku_1a9c847e249e47c?");
  
 // Get id from URL to delete that user
 $id = $_GET['id'];
